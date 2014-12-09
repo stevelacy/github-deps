aaa6e0163fd9a3fd10c7807887367246ce59df67
Steve Lacy <me@slacy.me>
2014-12-08 21:01:48 -0700

    reduce gh ratelimit api calls

--- test/main.js
+++ test/main.js
@@ -4,7 +4,6 @@ var should = require('should');
 describe('depss', function(){
   it('should get json from URL', function(done){
     depss('https://github.com/stevelacy/github-deps/blob/master/test/fixtures/pkg.json', {}, function(err, data){
-      console.log(data);
       should(err).equal(null);
       should.exist(data);
       should(data).be.type('object');
@@ -15,23 +14,16 @@ describe('depss', function(){
   });
 
   it('should get json from Array of URLs', function(done){
-    var urls = [
-      'https://github.com/stevelacy/github-deps/blob/master/test/fixtures/pkg.json',
-      'https://github.com/stevelacy/github-deps/blob/master/test/fixtures/pkg.json'
-    ];
+    var urls = ['https://github.com/stevelacy/github-deps/blob/master/test/fixtures/pkg.json'];
 
     depss(urls, {}, function(err, data){
       should(err).equal(null);
       should.exist(data);
       should(data).be.type('object');
       should.exist(data[0].deps.mocha);
-      should.exist(data[1].deps.mocha);
       should.exist(data[0].deps.mocha.update);
-      should.exist(data[1].deps.mocha.update);
       should(data[0].deps.mocha.update).be.greaterThan('1.1.1');
-      should(data[1].deps.mocha.update).be.greaterThan('1.1.1');
       should(data[0].deps.mocha.current).match(/1.1.0/);
-      should(data[1].deps.mocha.current).match(/1.1.0/);
       done();
     });
   });
