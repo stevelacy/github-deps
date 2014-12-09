731e28a281a6a50609dc15619162d4074c4774c8
Steve Lacy <me@slacy.me>
2014-12-08 20:15:33 -0700

    add tests

---
+++ test/main.js
@@ -0,0 +1,38 @@
+var depss = require('../');
+var should = require('should');
+
+describe('depss', function(){
+  it('should get json from URL', function(done){
+    depss('https://github.com/stevelacy/depss/blob/master/test/fixtures/pkg.json', {}, function(err, data){
+      console.log(data);
+      should(err).equal(null);
+      should.exist(data);
+      should(data).be.type('object');
+      should(data.deps.mocha.update).be.greaterThan('1.1.1');
+      should(data.deps.mocha.current).match(/1.1.0/);
+      done();
+    });
+  });
+
+  it('should get json from Array of URLs', function(done){
+    var urls = [
+      'https://github.com/stevelacy/depss/blob/master/test/fixtures/pkg.json',
+      'https://github.com/stevelacy/depss/blob/master/test/fixtures/pkg.json'
+    ];
+
+    depss(urls, {}, function(err, data){
+      should(err).equal(null);
+      should.exist(data);
+      should(data).be.type('object');
+      should.exist(data[0].deps.mocha);
+      should.exist(data[1].deps.mocha);
+      should.exist(data[0].deps.mocha.update);
+      should.exist(data[1].deps.mocha.update);
+      should(data[0].deps.mocha.update).be.greaterThan('1.1.1');
+      should(data[1].deps.mocha.update).be.greaterThan('1.1.1');
+      should(data[0].deps.mocha.current).match(/1.1.0/);
+      should(data[1].deps.mocha.current).match(/1.1.0/);
+      done();
+    });
+  });
+});
